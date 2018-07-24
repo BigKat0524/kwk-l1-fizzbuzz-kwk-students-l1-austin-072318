f530169ffa463bb40504956afec5955a27f7b860
BigKat0524 <katrinapartain.kk@gmail.com>
2018-07-24 18:46:11 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,10 @@
+def fizzbuzz (int)
+ if int % 5 == 0 && int % 3 == 0 
+   puts "fizzbuzz"
+elsif int % 5 == 0 
+   puts "fizz"
+elsif int % 3 == 0 
+  puts "buzz"
+ end 
+ end
+ fizzbuzz(15)
---
+++ style.rb
@@ -0,0 +1,11 @@
+def cupid_shuffle
+count = 20
+until count == 10
+puts "to the right, to the right, to the right"
+puts "to the left, to the left, to the left"
+puts "now kick, now kick, now kick"
+count -= 1
+puts "#{count} moves completed"
+end
+end 
+cupid_shuffle
